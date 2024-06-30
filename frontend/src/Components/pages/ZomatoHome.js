b70e0c90f6dd04e17da75f7310309fd4a537bf58
HarshPatiL11 <patilharsh497@gmail.com>
2024-06-30 13:35:31 +0530

    Second commit

---
+++ frontend/src/Components/pages/ZomatoHome.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "../Css/ZomatoHome.css"
+
+const ZomatoHome = () => {
+  return (
+    <>
+      <div className="ZomatoHome Container">
+           <div className='ZomatoHomeTitle'></div>
+           <div className='ZomatoHomeDescrition'></div>
+           <div className='ZomatoHomeDescrition'></div>
+      </div>
+    </>
+  )
+}
+
+export default ZomatoHome
