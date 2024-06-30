0799ed6ef12857979b7d0cd7ec79cba4f375faae
HarshPatiL11 <patilharsh497@gmail.com>
2024-06-30 16:01:26 +0530

    Third commit

--- frontend/src/Components/pages/ZomatoHome.js
+++ frontend/src/Components/pages/ZomatoHome.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import "../Css/ZomatoHome.css"
+import { FaLocationDot } from "react-icons/fa6";
+import { IoMdArrowDropdown } from "react-icons/io";
+import { HiOutlineMagnifyingGlass } from "react-icons/hi2";
+
 
 const ZomatoHome = () => {
-  return (
+  const ZomatoHomearry = [];
+  fe
+  return ( 
     <>
       <div className="ZomatoHome Container">
-           <div className='ZomatoHomeTitle'></div>
-           <div className='ZomatoHomeDescrition'></div>
-           <div className='ZomatoHomeDescrition'></div>
+       
+        <div className='ZomatoHomeTitle'><p className='HomeTitle'>Zomato</p></div>
+           <div className='ZomatoHomeDescrition'><p className='HomeDescrition'>Discover the best food & drinks in Mumbai</p></div>
+           <div className='ZomatoHomeSearch'><div className='HomeSearch'> <FaLocationDot/><div className='TextBox'>
+    <input type="text" placeholder="Thane,Maharastra " />
+  </div> <IoMdArrowDropdown/> </div>
+           <div className='SearchTextBox'> 
+           <div className='TextBox'>
+           <HiOutlineMagnifyingGlass/>
+    <input type="text" placeholder="Search for restraunt, cuisine or a dish" />
+  </div>
+           </div>
+           </div>
+   
+           
       </div>
     </>
   )
