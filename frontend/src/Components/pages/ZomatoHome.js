c0f887ebff1b7a280a5ee232f134bb8770f75848
HarshPatiL11 <patilharsh497@gmail.com>
2024-07-02 15:43:36 +0530

    API data Accessed

--- frontend/src/Components/pages/ZomatoHome.js
+++ frontend/src/Components/pages/ZomatoHome.js
@@ -1,34 +1,49 @@
-import React from 'react';
-import "../Css/ZomatoHome.css"
+import React, { useEffect, useState } from 'react';
+import "../Css/ZomatoHome.css";
 import { FaLocationDot } from "react-icons/fa6";
 import { IoMdArrowDropdown } from "react-icons/io";
 import { HiOutlineMagnifyingGlass } from "react-icons/hi2";
-
+import axios from "axios";
 
 const ZomatoHome = () => {
-  const ZomatoHomearry = [];
-    
-  return ( 
-    <>
-      <div className="ZomatoHome Container">
-       
-        <div className='ZomatoHomeTitle'><p className='HomeTitle'>Zomato</p></div>
-           <div className='ZomatoHomeDescrition'><p className='HomeDescrition'>Discover the best food & drinks in Mumbai</p></div>
-           <div className='ZomatoHomeSearch'><div className='HomeSearch'> <FaLocationDot className='LocationIcon'/><div className='TextBox'>
-    <input type="text" placeholder="Thane,Maharastra " />
-  </div> <IoMdArrowDropdown className='DownArrowIcon'/> </div>
-           <div className='SearchTextBox'> 
-           
-           <HiOutlineMagnifyingGlass/>
-      <input type="text" placeholder="Search for restraunt, cuisine or a dish" />
+  const [appHomeData, setAppHomeData] = useState([]);
 
-           </div>
-           </div>
-   
-           
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("http://localhost:8000/api/v1/menu/getMenu");
+        setAppHomeData(response.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return ( 
+    <div className="ZomatoHome Container">
+      <div className='ZomatoHomeTitle'>
+        <p className='HomeTitle'>{appHomeData.length > 0 ? appHomeData[0].heading : "Loading..."}</p>
       </div>
-    </>
-  )
+      <div className='ZomatoHomeDescrition'>
+        <p className='HomeDescrition'>{appHomeData.length > 0 ? appHomeData[0].description : ""}</p>
+      </div>
+      <div className='ZomatoHomeSearch'>
+        <div className='HomeSearch'>
+          <FaLocationDot className='LocationIcon'/>
+          <div className='TextBox'>
+            <input type="text" placeholder="Thane, Maharashtra " />
+          </div> 
+          <IoMdArrowDropdown className='DownArrowIcon'/> 
+        </div>
+        <div className='SearchTextBox'> 
+          <HiOutlineMagnifyingGlass/>
+          <input type="text" placeholder="Search for restaurant, cuisine or a dish" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default ZomatoHome
+export default ZomatoHome;
