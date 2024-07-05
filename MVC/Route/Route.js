742e5ba31b44f961a0e584d09e76e2c2ff8719cc
pratikshanhts2 <backenddevelopernhts2@gmail.com>
2024-07-05 23:17:46 +0530

    changes

--- MVC/Route/Route.js
+++ MVC/Route/Route.js
@@ -4,6 +4,11 @@ const router=express.Router()
 
 // menu
 router.post('/menu/newMenu', newMenus);     // POST 
-router.get('/menu/getMenu',getMenus);     // GET   
+router.get('/menu/getMenu',getMenus);     // GET  
+
+
+//locations
+router.post('/location/new-location', createLocation);     // POST 
+router.get('/location/list',getLocations);     // GET  
 
 export default router
