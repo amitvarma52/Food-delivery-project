db65badc8c3002e73b589843db7014297f381093
Amit varma <155515569+amitvarma52@users.noreply.github.com>
2024-07-07 14:27:26 +0530

    user api

--- MVC/Route/Route.js
+++ MVC/Route/Route.js
@@ -1,9 +1,13 @@
 import express from 'express'
 import { getMenus, newMenus } from '../Controllers/MenusController.js';
+import { getUser, newUser} from '../Controllers/SignUpController.js'
 const router=express.Router()
 
 // menu
-router.post('/menu/newMenu', newMenus);     // POST 
+router.post('/menu/newMenu',newMenus);   // POST 
 router.get('/menu/getMenu',getMenus);     // GET   
 
+// sign-up
+router.post('/user/newUser',newUser)     // POST
+router.get('/user/getUser', getUser);
 export default router
