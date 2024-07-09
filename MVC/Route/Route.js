f3c80bb09a34aebc1905c7ce8f6b44a8078b8188
HarshPatiL11 <patilharsh497@gmail.com>
2024-07-09 11:53:02 +0530

    added footer api

--- MVC/Route/Route.js
+++ MVC/Route/Route.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import { getMenus, newMenus } from '../Controllers/MenusController.js';
 import { getUser, newUser} from '../Controllers/SignUpController.js'
+import { createfooter, getfooter } from '../Controllers/FooterController.js';
 const router=express.Router()
 
 // menu
@@ -9,5 +10,10 @@ router.get('/menu/getMenu',getMenus);     // GET
 
 // sign-up
 router.post('/user/newUser',newUser)     // POST
-router.get('/user/getUser', getUser);
+router.get('/user/getUser', getUser);   //GET
+
+// Footer
+router.post("/footer/createfooter", createfooter); //POST
+router.get("/footer/getfooter", getfooter); //GET
+
 export default router
